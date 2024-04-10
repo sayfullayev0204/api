d9482ac60893b2a2587311955aaddd0d34bb2c8e
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-04-10 08:04:35 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apis/', include('apis.urls')),
+    path('', include('apis.urls')),
     path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
