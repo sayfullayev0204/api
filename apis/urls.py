88dd62b49a3996f6f4c4b67737d3a85ad965ed2b
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-04-10 07:42:37 +0500

    Update urls.py

--- apis/urls.py
+++ apis/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import TumanAPIView, MaktabAPIView, ShaxsAPIView
 
 urlpatterns = [
-    path('tumanlar/', TumanAPIView.as_view(), name='tumanlar-list'),
-    path('tumanlar/<int:tuman_id>/maktablar/', MaktabAPIView.as_view(), name='maktablar-list'),
-    path('shaxslar/', ShaxsAPIView.as_view(), name='shaxslar-list'),
+    path('tuman/', TumanAPIView.as_view(), name='tumanlar'),
+    path('tuman/<int:tuman_id>/maktab/', MaktabAPIView.as_view(), name='maktablar'),
+    path('tuman/<int:tuman_id>/shaxs/', ShaxsAPIView.as_view(), name='shaxslar'),
 ]
