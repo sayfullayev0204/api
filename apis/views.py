7621d77ec297cde233165d81ecf0e1d898d166f3
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-04-10 07:41:59 +0500

    Update views.py

--- apis/views.py
+++ apis/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Tuman, Maktab,Shaxs
-from .serializers import TumanSerializer, MaktabSerializer,ShaxsSerializer
+from .models import Tuman, Maktab, Shaxs
+from .serializers import TumanSerializer, MaktabSerializer, ShaxsSerializer
 
 class TumanAPIView(generics.ListAPIView):
     queryset = Tuman.objects.all()
@@ -13,6 +13,13 @@ class MaktabAPIView(generics.ListCreateAPIView):
         tuman_id = self.kwargs['tuman_id']
         return Maktab.objects.filter(tuman_id=tuman_id)
 
-class ShaxsAPIView(generics.ListAPIView):
-    queryset = Shaxs.objects.all()
+class ShaxsAPIView(generics.ListCreateAPIView):
     serializer_class = ShaxsSerializer
+
+    def get_queryset(self):
+        tuman_id = self.kwargs['tuman_id']
+        return Shaxs.objects.filter(tuman_id=tuman_id)
+
+    def perform_create(self, serializer):
+        tuman_id = self.kwargs['tuman_id']
+        serializer.save(tuman_id=tuman_id)
